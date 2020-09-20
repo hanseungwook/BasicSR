af2aa011c0b4bf9c67a4925fdf61010014cee57c
Seungwook Han <sh3264@coumbia.edu>
2020-09-20 18:52:22 +0900

    Excluding 4 channel images from lmdb

--- basicsr/data/util.py
+++ basicsr/data/util.py
@@ -141,14 +141,20 @@ def paired_paths_from_lmdb(folders, keys):
         input_lmdb_keys = [line.split('.')[0] for line in fin]
         fin.seek(0)
         input_lmdb_shapes = [line.split('.')[1] for line in fin]
+        # Indices of 1 channel
         c3_idxs = [0 if ',1)' in input_shape else 1 for input_shape in input_lmdb_shapes]
+        # Including indices of 4 channel
+        c3_idxs = [0 if ',4)' in input_lmdb_shapes[k] else c3_idxs[k] for k in range(len(input_lmdb_shapes))]
         input_lmdb_keys = [input_lmdb_keys[j] for j in range(len(input_lmdb_shapes)) if c3_idxs[j] == 1]
+
     with open(osp.join(gt_folder, 'meta_info.txt')) as fin:
         gt_lmdb_keys = [line.split('.')[0] for line in fin]
         fin.seek(0)
         gt_lmdb_shapes = [line.split('.')[1] for line in fin]
         c3_idxs = [0 if ',1)' in input_shape else 1 for input_shape in gt_lmdb_shapes]
+        c3_idxs = [0 if ',4)' in gt_lmdb_shapes[k] else c3_idxs[k] for k in range(len(gt_lmdb_shapes))]
         gt_lmdb_keys = [gt_lmdb_keys[j] for j in range(len(gt_lmdb_shapes)) if c3_idxs[j] == 1]
+
     if set(input_lmdb_keys) != set(gt_lmdb_keys):
         raise ValueError(
             f'Keys in {input_key}_folder and {gt_key}_folder are different.')
