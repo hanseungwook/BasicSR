66477cfeca5253d0168e8bb5307b5e1e8ba41199
Seungwook Han <sh3264@coumbia.edu>
2020-09-23 17:53:00 +0900

    Different normalization values for pretraind model

--- basicsr/models/archs/srresnet_wt_arch.py
+++ basicsr/models/archs/srresnet_wt_arch.py
@@ -68,10 +68,10 @@ class MSRResNet_WT_Pixel(nn.Module):
         # Normalization buffers
         self.register_buffer(
             'shift',
-            torch.Tensor([3.0]))
+            torch.Tensor([2.0]))
         self.register_buffer(
             'scale',
-                torch.Tensor([10.0]))
+                torch.Tensor([8.0]))
 
     def forward(self, x):
         # IWT x once to get LFC
