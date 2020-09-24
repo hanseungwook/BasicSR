9a6b6473b9b452428c9562484364b882c52cfc62
Seungwook Han <sh3264@coumbia.edu>
2020-09-24 12:49:40 +0900

    lmdb creation for interpolation + wt

--- basicsr/utils/lmdb.py
+++ basicsr/utils/lmdb.py
@@ -549,8 +549,139 @@ def make_wt_lmdb_from_imgs(data_path,
     txt_file.close()
     print('\nFinish writing lmdb.')
 
+def make_inter_wt_lmdb_from_imgs(data_path,
+                        lmdb_path,
+                        img_path_list,
+                        keys,
+                        batch=5000,
+                        compress_level=1,
+                        multiprocessing_read=False,
+                        n_thread=40,
+                        map_size=None):
+    """Make lmdb from images.
 
-def read_img_worker(path, key, compress_level, lr=False, use_wt=False, filters=None):
+    Contents of lmdb. The file structure is:
+    example.lmdb
+    ├── data.mdb
+    ├── lock.mdb
+    ├── meta_info.txt
+
+    The data.mdb and lock.mdb are standard lmdb files and you can refer to
+    https://lmdb.readthedocs.io/en/release/ for more details.
+
+    The meta_info.txt is a specified txt file to record the meta information
+    of our datasets. It will be automatically created when preparing
+    datasets by our provided dataset tools.
+    Each line in the txt file records 1)image name (with extension),
+    2)image shape, and 3)compression level, separated by a white space.
+
+    For example, the meta information could be:
+    `000_00000000.png (720,1280,3) 1`, which means:
+    1) image name (with extension): 000_00000000.png;
+    2) image shape: (720,1280,3);
+    3) compression level: 1
+
+    We use the image name without extension as the lmdb key.
+
+    If `multiprocessing_read` is True, it will read all the images to memory
+    using multiprocessing. Thus, your server needs to have enough memory.
+
+    Args:
+        data_path (str): Data path for reading images.
+        lmdb_path (str): Lmdb save path.
+        img_path_list (str): Image path list.
+        keys (str): Used for lmdb keys.
+        batch (int): After processing batch images, lmdb commits.
+            Default: 5000.
+        compress_level (int): Compress level when encoding images. Default: 1.
+        multiprocessing_read (bool): Whether use multiprocessing to read all
+            the images to memory. Default: False.
+        n_thread (int): For multiprocessing.
+        map_size (int | None): Map size for lmdb env. If None, use the
+            estimated size from images. Default: None
+    """
+
+    assert len(img_path_list) == len(keys), (
+        'img_path_list and keys should have the same length, '
+        f'but got {len(img_path_list)} and {len(keys)}')
+    print(f'Create lmdb for {data_path}, save to {lmdb_path}...')
+    print(f'Totoal images: {len(img_path_list)}')
+    if not lmdb_path.endswith('.lmdb'):
+        raise ValueError("lmdb_path must end with '.lmdb'.")
+    if osp.exists(lmdb_path):
+        print(f'Folder {lmdb_path} already exists. Exit.')
+        sys.exit(1)
+
+    if multiprocessing_read:
+        # read all the images to memory (multiprocessing)
+        dataset = {}  # use dict to keep the order for multiprocessing
+        shapes = {}
+        print(f'Read images with multiprocessing, #thread: {n_thread} ...')
+        pbar = ProgressBar(len(img_path_list))
+
+        def callback(arg):
+            """get the image data and update pbar."""
+            key, dataset[key], shapes[key] = arg
+            pbar.update('Reading {}'.format(key))
+
+        pool = Pool(n_thread)
+        for path, key in zip(img_path_list, keys):
+            pool.apply_async(
+                read_img_worker,
+                args=(osp.join(data_path, path), key, compress_level),
+                callback=callback)
+        pool.close()
+        pool.join()
+        print(f'Finish reading {len(img_path_list)} images.')
+
+    # create wt filters
+    filters = create_filters(device='cpu')
+
+    # create lmdb environment
+    if map_size is None:
+        # obtain data size for one image
+        img = mmcv.imread(
+            osp.join(data_path, img_path_list[0]), flag='unchanged')
+        img = mmcv.image.imresize(img, (128, 128), interpolation='lanczos', backend='cv2')
+        img = torch.from_numpy(img / 255.0).float()
+        img = wt(img.permute(2,0,1).unsqueeze(0), filters, levels=2)[:, :, :64, :64].numpy()
+        img_byte = img.tobytes()
+        data_size_per_img = len(img_byte)
+        print('Data size per image is: ', data_size_per_img)
+        data_size = data_size_per_img * len(img_path_list)
+        map_size = data_size * 10
+
+    env = lmdb.open(lmdb_path, map_size=map_size)
+
+    
+    # write data to lmdb
+    pbar = ProgressBar(len(img_path_list))
+    txn = env.begin(write=True)
+    txt_file = open(osp.join(lmdb_path, 'meta_info.txt'), 'w')
+    for idx, (path, key) in enumerate(zip(img_path_list, keys)):
+        pbar.update(f'Write {key}')
+        key_byte = key.encode('ascii')
+        if multiprocessing_read:
+            img_byte = dataset[key]
+            h, w, c = shapes[key]
+        else:
+            _, img_byte, img_shape = read_img_worker(
+                osp.join(data_path, path), key, compress_level, use_wt=False, use_inter_wt=True, filters=filters)
+            h, w, c = img_shape
+
+        txn.put(key_byte, img_byte)
+        # write meta information
+        txt_file.write(f'{key}.png ({h},{w},{c}) {compress_level}\n')
+        if idx % batch == 0:
+            txn.commit()
+            txn = env.begin(write=True)
+    txn.commit()
+    env.close()
+    txt_file.close()
+    print('\nFinish writing lmdb.')
+
+
+def read_img_worker(path, key, compress_level, lr=False, use_wt=False, use_inter_wt=False, filters=None):
     """Read image worker.
 
     Args:
@@ -578,6 +709,14 @@ def read_img_worker(path, key, compress_level, lr=False, use_wt=False, filters=N
             img = wt(img.unsqueeze(0).unsqueeze(0), filters, levels=3)[:, :, :64, :64].squeeze().numpy()
         elif img.ndim == 3:
             img = wt(img.permute(2,0,1).unsqueeze(0), filters, levels=3)[:, :, :64, :64].squeeze().permute(1,2,0).numpy()
+    elif use_inter_wt:
+        img = mmcv.image.imresize(img, (128, 128), interpolation='lanczos', backend='cv2')
+        if img.ndim == 2:
+            print(img.shape, 'weird shape of image c=1')
+            sys.exit(1)
+            img = wt(img.unsqueeze(0).unsqueeze(0), filters, levels=2)[:, :, :64, :64].squeeze().numpy()
+        elif img.ndim == 3:
+            img = wt(img.permute(2,0,1).unsqueeze(0), filters, levels=2)[:, :, :64, :64].squeeze().permute(1,2,0).numpy()
 
     if img.ndim == 2:
         h, w = img.shape
