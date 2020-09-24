2b8f1df4585d6905abb84f50330ee064e02c9629
Seungwook Han <sh3264@coumbia.edu>
2020-09-24 15:16:07 +0900

    Separated single numpy wt and downsampled dataset and created run script for esrgan pixel with biggan samples

--- basicsr/data/single_numpy_wt_dataset.py
+++ basicsr/data/single_numpy_wt_dataset.py
@@ -7,10 +7,10 @@ from basicsr.data.transforms import augment, paired_random_crop, totensor
 from basicsr.models.archs import arch_util
 
 
-class SingleNumpyDataset(data.Dataset):
-    """Numpy single image dataset.
+class SingleNumpyWTDataset(data.Dataset):
+    """Numpy single image (WT) dataset.
 
-    Read 64x64 downsampled (using DUF) images
+    Read 64x64 WT images (need to apply one more WT operation in order to split it into 4 quadrants as the models expect)
 
 
     Args:
@@ -19,14 +19,20 @@ class SingleNumpyDataset(data.Dataset):
     """
 
     def __init__(self, opt):
-        super(SingleNumpyDataset, self).__init__()
+        super(SingleNumpyWTDataset, self).__init__()
         self.opt = opt
 
         self.lq_folder = opt['dataroot_lq']
         self.lq_dataset = np.load(self.lq_folder)['x']
+        
+        self.filters = arch_util.create_filters()
 
     def __getitem__(self, index):
-        return {'lq': self.lq_dataset[index], 'lq_path': self.lq_folder.split('.')[0] + '_{}.png'.format(index)}
+        # Apply 1 more WT
+        img = self.lq_dataset[index]
+        img_wt = arch_util.wt(torch.from_numpy(img).unsqueeze(0), self.filters, levels=1).squeeze()
+
+        return {'lq': img_wt, 'lq_path': self.lq_folder.split('.')[0] + '_{}.png'.format(index)}
 
     def __len__(self):
         return self.lq_dataset.shape[0]
