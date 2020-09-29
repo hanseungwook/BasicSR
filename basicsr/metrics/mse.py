76dee09608a6cc7c12a1cc88dcf5a251544ee1b7
Seungwook Han <sh3264@coumbia.edu>
2020-09-29 10:50:25 +0900

    Added nll (mse) loss calculation

---
+++ basicsr/metrics/mse.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def calculate_mse(img1, img2):
+    img1 = img1.astype(np.float32) / 255.
+    img2 = img1.astype(np.float32) / 255.
+
+    return np.square(np.subtract(img1, img2)).mean()
+
